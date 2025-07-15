79c24a39696004ceae76d20d701e2bce75fa5ce9
Yuubbe <120384600+Yuubbe@users.noreply.github.com>
2025-07-16 01:09:58 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Candidature Blue Lock Yuu',
   description: 'Dev by Yuu',
-    generator: 'v0.dev'
+   
 }
 
 export default function RootLayout({
